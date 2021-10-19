6f56ad6fb4bedc5f01516cfd55e7a4693d78108c
Jackson_Dostal <jacksondostal@gmail.com>
2021-10-19 12:44:15 -0400

    python set up

---
+++ main.py
@@ -0,0 +1,9 @@
+name = "Jackson"
+eyeColour = "blue"
+fingers = int(10)
+score = int(0)
+
+print ("---------------------------------------")
+print ("My name is " + name + " and my eyes are " + eyeColour)
+print ("I still have " + str(fingers) + " fingers and " + str(fingers) + " toes!")
+print ("---------------------------------------")
