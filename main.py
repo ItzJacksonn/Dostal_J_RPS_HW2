9f9864162e1a8c663b0b034392276416da2b120a
Jackson_Dostal <jacksondostal@gmail.com>
2021-10-19 14:37:15 -0400

    majority of RPS game is finished

--- main.py
+++ main.py
@@ -1,9 +1,47 @@
-name = "Jackson"
-eyeColour = "blue"
-fingers = int(10)
-score = int(0)
+from random import randint
 
-print ("---------------------------------------")
-print ("My name is " + name + " and my eyes are " + eyeColour)
-print ("I still have " + str(fingers) + " fingers and " + str(fingers) + " toes!")
-print ("---------------------------------------")
+playerScore = 0
+computerScore = 0
+
+print("Welcome to the Rock, Paper, Scissors Game.")
+print("First to three points wins")
+print("--------------------------------------------")
+
+if (playerScore == 3):
+	print("Congratulaions, You Won The Game")
+elif (computerScore == 3):
+	print("Sorry, You Lost The Game")
+else:
+	player = input("Choose rock, paper, or scissors: ")
+	print("Player chose: " + player)
+
+	choices = ["rock", "paper", "scissors"]
+	computer = choices[randint(0,2)]
+	print("Computer chose: " + computer)
+
+	if (computer == player):
+		print("Its A Tie!")
+	elif (player == "rock"):
+		if (computer == "scissors"):
+			print("Player wins!")
+			playerScore = playerScore + 1
+		else:
+			print("Computer wins!")
+			computerScore = computerScore + 1
+	elif (player == "paper"):
+		if (computer == "rock"):
+			print("Player wins!")
+			playerScore = playerScore + 1
+		else:
+			print("Computer wins!")
+			computerScore = computerScore + 1
+	elif (player == "scissors"):
+		if (computer == "paper"):
+			print("Player wins!")
+			playerScore = playerScore + 1
+		else:
+			print("Computer wins!")
+			computerScore = computerScore + 1
+
+print("Player score is " + str(playerScore))
+print("Computer score is " + str(computerScore))
